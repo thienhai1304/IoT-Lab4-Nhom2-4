4622a11ae2aec6f03ceb98b68d767ec2e227de93
thienhai1304 <thienhai1011@gmail.com>
2022-05-24 15:14:01 +0700

    add

--- src/backend/controllers/user.controller.js
+++ src/backend/controllers/user.controller.js
@@ -7,29 +7,38 @@ exports.userAuth = (req, res) => {
 exports.getDataTemp = (req, res) => {
     Device.findOne(
         {type: 'temp'},
-        {value: 1, _id: 0}).sort({time: '-1'})
+        {value: 1,  time: 1, _id: 0}).sort({time: '-1'})
     .then(data => {
         console.log(data)
         return res.status(200).send(data)
+    })
+    .catch(e => {
+        console.log(e)
     })
 }
 
 exports.getDataHumid = (req, res) => {
     Device.findOne(
         {type: 'humid'},
-        {value: 1, _id: 0}).sort({time: '-1'})
+        {value: 1, time: 1, _id: 0}).sort({time: '-1'})
     .then(data => {
         console.log(data)
         return res.status(200).send(data)
+    })
+    .catch(e => {
+        console.log(e)
     })
 }
 
 exports.getDataLux = (req, res) => {
     Device.findOne(
         {type: 'lux'},
-        {value: 1, _id: 0}).sort({time: '-1'})
+        {value: 1,  time: 1, _id: 0}).sort({time: '-1'})
     .then(data => {
         console.log(data)
         return res.status(200).send(data)
+    })
+    .catch(e => {
+        console.log(e)
     })
 }
--- src/frontend/assets/js/chart.js
+++ src/frontend/assets/js/chart.js
@@ -1,81 +1,217 @@
 var URL = 'http://localhost:5500'
 
-function getData() {
-    //fetch(URL + /get/data/temp)
+const ctx_bar = document.getElementById("chart_bar").getContext('2d');
+const ctx_line = document.getElementById("chart_line").getContext('2d');
+
+var myChart_bar 
+var myChart_line
+
+var dataTemp
+var dataHumid
+var dataLight
+var labelTimeline
+
+function getDataTemp() {
+    fetch(URL + '/get/data/temp', {
+        'Access-Control-Allow-Headers': 'X-Requested-With',
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type': 'application/json',
+    })
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        dataTemp = data.value
+    })
+    .then(() => {
+        return getDataHumid()
+    })
+    .catch(e => {
+        console.log(e)
+    })
+}
+
+function getDataHumid() {
+    fetch(URL + '/get/data/humid', {
+        'Access-Control-Allow-Headers': 'X-Requested-With',
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type': 'application/json',
+    })
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        dataHumid = data.value
+    })
+    .then(() => {
+        return getDataLux()
+    })
+    .catch(e => {
+        console.log(e)
+    })
+}
+
+function getDataLux() {
+    fetch(URL + '/get/data/lux', {
+        'Access-Control-Allow-Headers': 'X-Requested-With',
+        'Access-Control-Allow-Origin' : '*',
+        'Content-Type': 'application/json',
+    })
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        dataLight = data.value
+    })
+    .catch(e => {
+        console.log(e)
+    })
 }
 
 // ------------- BAR CHART ------------- 
 
-const ctx_bar = document.getElementById("chart_bar").getContext('2d');
-const myChart_bar = new Chart(ctx_bar, {
-    type: 'bar',
-    data: {
-        labels: ["Today"],
-        datasets: [{
-            label: 'Temperature',
-            backgroundColor: 'rgba(255, 99, 132, 1)',
-            borderColor: 'rgb(47, 128, 237)',
-            data: [30],
+// First draw
+drawChartBar()
+
+// Update chart
+setInterval(updateChartBar, 1000)
+
+function updateChartBar() {
+    var date = new Date()
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+    
+    labelTimeline = `${hour}:${minute}:${second}`
+
+    getTotalData()
+    addDataBar(myChart_bar, labelTimeline)
+}
+
+function addDataBar(chart, label) {
+    chart.data.labels[0] = label;
+    console.log(`${dataTemp} ${dataHumid} ${dataLight}`)
+    chart.data.datasets[0].data[0] = dataTemp
+    chart.data.datasets[1].data[0] = dataHumid
+    chart.data.datasets[2].data[0] = dataLight
+    chart.update();
+}
+
+function drawChartBar() {
+    myChart_bar = new Chart(ctx_bar, {
+        type: 'bar',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Temperature',
+                backgroundColor: 'rgba(255, 99, 132, 1)',
+                borderColor: 'rgb(47, 128, 237)',
+                data: [],
+            },
+            {
+                label: 'Humidity',
+                backgroundColor: 'rgba(54, 162, 235, 1)',
+                borderColor: 'rgb(47, 128, 237)',
+                data: [],
+            },
+            {
+                label: 'Light',
+                backgroundColor: 'rgba(255, 206, 86, 1)',
+                borderColor: 'rgb(47, 128, 237)',
+                data: [],
+            }]
         },
-        {
-            label: 'Humidity',
-            backgroundColor: 'rgba(54, 162, 235, 1)',
-            borderColor: 'rgb(47, 128, 237)',
-            data: [60],
-        },
-        {
-            label: 'Light',
-            backgroundColor: 'rgba(255, 206, 86, 1)',
-            borderColor: 'rgb(47, 128, 237)',
-            data: [200],
-        }]
-    },
-    options: {
-        scales: {
-        yAxes: [{
-            ticks: {
-            beginAtZero: true,
+        options: {
+            scales: {
+            yAxes: [{
+                ticks: {
+                beginAtZero: true,
+                }
+            }]
             }
-        }]
-        }
-    },
-});
+        },
+    });
+}
 
 // ------------- LINE CHART -------------
 
-const ctx_line = document.getElementById("chart_line").getContext('2d');
-const myChart_line = new Chart(ctx_line, {
-    type: 'line',
-    data: {
-    labels: ["sunday", "monday", "tuesday",
-    "wednesday", "thursday", "friday", "saturday",],
-    datasets: [{
-        label: 'Temperature',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor:'rgba(255, 99, 132, 1)',
-        data: [15, 30, 40, 20, 50, 10, 25],
-    },
-    {
-        label: 'Humidity',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        data: [20, 100, 30, 50, 70, 40, 10],
-    },
-    {
-        label: 'Light',
-        backgroundColor: 'rgba(0, 0, 0, 0)',
-        borderColor: 'rgba(255, 206, 86, 1)',
-        data: [200, 500, 300, 100, 400, 200, 100],
+// First draw
+drawChartLine()
+
+// Update chart
+setInterval(updateChartLine, 1000)
+function updateChartLine() {
+    /* var date = new Date()
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+    
+    labelTimeline = `${hour}:${minute}:${second}` */
+
+    //getTotalData()
+    addDataLine(myChart_line, labelTimeline)
+
+    if (myChart_line.data.labels.length > 10) {
+        removeData(myChart_line)
     }
-    ]
-    },
-    options: {
-    scales: {
-        yAxes: [{
-        ticks: {
-            beginAtZero: true,
+}
+
+function addDataLine(chart, label) {
+    chart.data.labels.push(label);
+    console.log(`${dataTemp} ${dataHumid} ${dataLight}`)
+    chart.data.datasets[0].data.push(dataTemp)
+    chart.data.datasets[1].data.push(dataHumid)
+    chart.data.datasets[2].data.push(dataLight)
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.shift();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.shift();
+    });
+    chart.update();
+}
+
+function getTotalData() {
+    getDataTemp()
+    getDataHumid()
+    getDataLux()
+}
+
+function drawChartLine() {
+    myChart_line = new Chart(ctx_line, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Temperature',
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                borderColor:'rgba(255, 99, 132, 1)',
+                data: [],
+            },
+            {
+                label: 'Humidity',
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                data: [],
+            },
+            {
+                label: 'Light',
+                backgroundColor: 'rgba(0, 0, 0, 0)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                data: [],
+            }
+            ]
+        },
+        options: {
+        scales: {
+            yAxes: [{
+            ticks: {
+                beginAtZero: false,
+            }
+            }]
         }
-        }]
-    }
-    },
-});
+        },
+    });
+}
