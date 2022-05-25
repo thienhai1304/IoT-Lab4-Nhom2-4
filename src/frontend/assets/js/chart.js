81e6778d318095bfe12dbafedad873501a6e0753
HuyyHoang <19521543@gm.uit.edu.vn>
2022-05-25 15:26:30 +0700

    hoang30cm

--- src/frontend/assets/js/chart.js
+++ src/frontend/assets/js/chart.js
@@ -220,15 +220,13 @@ function drawChartLine() {
 
 
 const myChart_pie = new Chart(ctx_pie, {
-type: 'pie',
-data: {
-    labels: ["Temperature", "Humidity", "Light"],
-    datasets: [{
-        backgroundColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
-        borderColor: 'rgb(47, 128, 237)',
-        data: [100, 200, 360],
+    type: 'pie',
+    data: {
+        labels: ["Temperature", "Humidity", "Light"],
+        datasets: [{
+            backgroundColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+            borderColor: 'rgb(47, 128, 237)',
+            data: [100, 200, 360],
+        }]
     },
-    
-]
-},
 });
